3bf984a09d0d70f1aace78fc2e7beafc6db2bfd4
Zahed <zahedm45@gmail.com>
2020-09-28 12:38:25 +0200

    Initial commit

---
+++ src/loop3/loop3.java
@@ -0,0 +1,12 @@
+package loop3;
+
+public class loop3 {
+    public static void main(String[] args) {
+
+        for(int num2 = 0; num2 <= 1;  num2++)
+            for(int num1 = 0; num1 <= 1; num1++)
+                for(int num3 = 0; num3 <= 1; num3++)
+                    System.out.println(num2 + "   " + num1 + "   " + num3);
+
+    }
+}
