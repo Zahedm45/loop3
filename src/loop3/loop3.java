d5771dc11e13c10ff49113200cb54e69972ac2cc
Zahedm45 <33806508+Zahedm45@users.noreply.github.com>
2020-09-28 12:40:19 +0200

    Update loop3.java
    num3 has been changed for 0 to 1.

--- src/loop3/loop3.java
+++ src/loop3/loop3.java
@@ -5,7 +5,7 @@ public class loop3 {
 
         for(int num2 = 0; num2 <= 1;  num2++)
             for(int num1 = 0; num1 <= 1; num1++)
-                for(int num3 = 0; num3 <= 1; num3++)
+                for(int num3 = 1; num3 <= 1; num3++) // Here the code has been changed.
                     System.out.println(num2 + "   " + num1 + "   " + num3);
 
     }
